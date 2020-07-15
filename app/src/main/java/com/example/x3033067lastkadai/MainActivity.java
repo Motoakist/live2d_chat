110b52784e81555e0a1ca1774a527b8f7b2d40bd
Motoakist <45988963+Motoakist@users.noreply.github.com>
2020-07-15 12:45:23 +0900

    7/15まで。speechrecognizer実機で動作確認

--- app/src/main/java/com/example/x3033067lastkadai/MainActivity.java
+++ app/src/main/java/com/example/x3033067lastkadai/MainActivity.java
@@ -7,7 +7,50 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+
 public class MainActivity extends AppCompatActivity {
+    private static String TAG = "Sample";
+    private SpeechRecognizer mRecognizer;
+    private RecognitionListener mRecognitionListener = new RecognitionListener() {
+        @Override
+        public void onError(int error) {
+            if ((error == SpeechRecognizer.ERROR_NO_MATCH) ||
+                    (error == SpeechRecognizer.ERROR_SPEECH_TIMEOUT)) {
+                startSpeechRecognition();
+                return;
+            }
+            Log.d(TAG, "Recognition Error: " + error);
+        }
+
+        @Override
+        public void onResults(Bundle results) {
+            ArrayList<String> values = results
+                    .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            String val = values.get(0);
+            Log.d(TAG, "認識結果: " + val);
+            startSpeechRecognition();
+        }
+
+        @Override public void onBeginningOfSpeech() {}
+        @Override public void onBufferReceived(byte[] arg0) {}
+        @Override public void onEndOfSpeech() {}
+        @Override public void onEvent(int arg0, Bundle arg1) {}
+        @Override public void onPartialResults(Bundle arg0) {}
+        @Override public void onReadyForSpeech(Bundle arg0) {}
+        @Override public void onRmsChanged(float arg0) {}
+    };
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,4 +77,24 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+
+
+    private void startSpeechRecognition() {
+        // Need to destroy a recognizer to consecutive recognition?
+        if (mRecognizer != null) {
+            mRecognizer.destroy();
+        }
+
+        // Create a recognizer.
+        mRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        mRecognizer.setRecognitionListener(mRecognitionListener);
+
+        // Start recognition.
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        mRecognizer.startListening(intent);
+    }
+
+
 }
