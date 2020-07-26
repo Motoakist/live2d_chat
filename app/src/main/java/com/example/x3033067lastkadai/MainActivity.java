d2f3d12b6578def55ebf5a98ffc333f9436d0fbc
Motoakist <45988963+Motoakist@users.noreply.github.com>
2020-07-26 11:25:35 +0900

    音声入力とlive2dを同時実装完了。

--- app/src/main/java/com/example/x3033067lastkadai/MainActivity.java
+++ app/src/main/java/com/example/x3033067lastkadai/MainActivity.java
@@ -1,11 +1,18 @@
+
+/**
+ * Copyright(c) Live2D Inc. All rights reserved.
+ * <p>
+ * Use of this source code is governed by the Live2D Open Software license
+ * that can be found at https://www.live2d.com/eula/live2d-open-software-license-agreement_en.html.
+ */
+
 package com.example.x3033067lastkadai;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.opengl.GLSurfaceView;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.speech.RecognitionListener;
@@ -13,18 +20,23 @@ import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 import java.util.Locale;
 
 import static android.Manifest.permission.RECORD_AUDIO;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
     private static final int MILLIS_IN_FUTURE = 11 * 500;
     private static final int COUNT_DOWN_INTERVAL = 1000;
 
@@ -36,11 +48,41 @@ public class MainActivity extends AppCompatActivity {
     private TextView countTextView;
     private CountDownTimer countDownTimer;
 
+    private GLSurfaceView _glSurfaceView;
+    private GLRenderer _glRenderer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        JniBridgeJava.SetActivityInstance(this);
+        JniBridgeJava.SetContext(this);
+        _glSurfaceView = new GLSurfaceView(this);
+        _glSurfaceView.setEGLContextClientVersion(2);
+        _glRenderer = new GLRenderer();
+        _glSurfaceView.setRenderer(_glRenderer);
+        _glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+        setContentView(_glSurfaceView);
+//        setContentView(R.layout.activity_main);
+        View view = this.getLayoutInflater().inflate(R.layout.activity_main, null);
+        addContentView(view,new LinearLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT
+                        ? View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        : View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        );
+
+
+
+
+        //ここから音声認識
         textView = (TextView) findViewById(R.id.textView);
         countTextView = (TextView) findViewById(R.id.countTextViwe);
 
@@ -64,6 +106,7 @@ public class MainActivity extends AppCompatActivity {
 
         // 音声認識スタートボタン
         Button button = (Button) findViewById(R.id.buttonView);
+        //ここが原因で落ちる...
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -83,6 +126,58 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        JniBridgeJava.nativeOnStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        _glSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        _glSurfaceView.onPause();
+        JniBridgeJava.nativeOnPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        JniBridgeJava.nativeOnStop();
+    }
+
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        JniBridgeJava.nativeOnDestroy();
+//    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float pointX = event.getX();
+        float pointY = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                JniBridgeJava.nativeOnTouchesBegan(pointX, pointY);
+                break;
+            case MotionEvent.ACTION_UP:
+                JniBridgeJava.nativeOnTouchesEnded(pointX, pointY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                JniBridgeJava.nativeOnTouchesMoved(pointX, pointY);
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+
+
+    //言語処理について
     private RecognitionListener recognitionListener = new RecognitionListener() {
         @Override
         public void onReadyForSpeech(Bundle bundle) {
@@ -218,5 +313,4 @@ public class MainActivity extends AppCompatActivity {
         }
         countDownTimer = null;
     }
-
 }
