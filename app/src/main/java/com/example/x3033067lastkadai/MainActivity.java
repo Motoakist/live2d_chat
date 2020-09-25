113eb2f5a2dc5d86d4d7033f87534dae3322ed0c
Motoakist <45988963+Motoakist@users.noreply.github.com>
2020-09-25 21:05:09 +0900

    極性辞書の調整

--- app/src/main/java/com/example/x3033067lastkadai/MainActivity.java
+++ app/src/main/java/com/example/x3033067lastkadai/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends Activity {
 
     private int expression_counter;
 
-    int dic_len = 55115;
+    int dic_len = 55112;
     String[] dic_w = new String[dic_len];
     String[] dic_n = new String[dic_len];
 
